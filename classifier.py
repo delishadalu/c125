ca52640d618c9cf24997ba9f555a3f3d64f90078
delishadalu <73183936+delishadalu@users.noreply.github.com>
2022-08-05 11:40:07 +0530

    Add files via upload

---
+++ classifier.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from PIL import Image
+import PIL.ImageOps
+
+
+def get_prediction(image):
+    im_pil = Image.open(image)
+    image_bw = im_pil.convert('L')
+    image_bw_resized = image_bw.resize((28, 28), Image.ANTIALIAS)
+    pixel_filter = 20
+    min_pixel = np.percentile(image_bw_resized, pixel_filter)
+    image_bw_resized_inverted_scaled = np.clip(
+        image_bw_resized-min_pixel, 0, 255)
+    max_pixel = np.max(image_bw_resized)
+    image_bw_resized_inverted_scaled = np.asarray(
+        image_bw_resized_inverted_scaled)/max_pixel
+    test_sample = np.array(image_bw_resized_inverted_scaled).reshape(1, 784)
+    test_pred = model.predict(test_sample)
+    return test_pred[0]
